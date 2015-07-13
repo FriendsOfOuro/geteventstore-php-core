587d95f890e17bc6d62af35e8ef088c82f6c93c6
Davide Bellettini <davide@bellettini.me>
2015-07-13 19:45:34 +0200

    Optimize download of EventStore using cache on Travis

---
+++ travis/download-eventstore.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function usage {
+    >&2 echo "Usage: $0 version"
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 2
+fi
+
+file="EventStore-OSS-Linux-$1.tar.gz"
+
+if [ ! -f "$HOME/downloads/$file" ]; then
+    echo "File $file not cached, downloading..."
+    wget "http://download.geteventstore.com/binaries/$file" -O "$HOME/downloads/$file"
+else
+    echo "File $file found in cache"
+    cp "$HOME/downloads/$file" .
+fi
