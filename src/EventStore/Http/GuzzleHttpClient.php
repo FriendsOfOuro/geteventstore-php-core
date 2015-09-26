d99a8ed79c37f9342b4d867f0bbcb915ca3d68c9
Davide Bellettini <davide@bellettini.me>
2015-09-26 12:49:49 +0200

    Enable EventStore to read event batches

--- src/EventStore/Http/GuzzleHttpClient.php
+++ src/EventStore/Http/GuzzleHttpClient.php
@@ -1,17 +1,38 @@
 <?php
 namespace EventStore\Http;
 
+use Exception as PhpException;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\ClientException as GuzzleClientException;
 use GuzzleHttp\Exception\RequestException as GuzzleRequestException;
+use GuzzleHttp\Handler\CurlMultiHandler;
+use GuzzleHttp\Pool;
 use Psr\Http\Message\RequestInterface;
 
 final class GuzzleHttpClient implements HttpClientInterface
 {
     public function __construct(ClientInterface $client = null)
     {
-        $this->client = $client ?: new Client();
+        $this->client = $client ?: new Client([
+            'handler' => new CurlMultiHandler(),
+        ]);
+    }
+
+    public function sendRequestBatch(array $requests)
+    {
+        $responses = Pool::batch(
+            $this->client,
+            $requests
+        );
+
+        foreach ($responses as $response) {
+            if ($response instanceof PhpException) {
+                throw $response;
+            }
+        }
+
+        return $responses;
     }
 
     public function send(RequestInterface $request)
