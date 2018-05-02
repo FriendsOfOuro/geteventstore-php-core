f059321706e13221bdae02e216c8350b55a8d726
Davide Bellettini <davide@bellettini.eu>
2018-05-02 22:45:10 +0200

    Update file URL

--- travis/download-eventstore.sh
+++ travis/download-eventstore.sh
@@ -9,12 +9,12 @@ if [ "$#" -ne 1 ]; then
     exit 2
 fi
 
-file="EventStore-OSS-$1.tar.gz"
+file="EventStore-OSS-Ubuntu-14.04-$1.tar.gz"
 
 if [ ! -f "$HOME/downloads/$file" ]; then
     echo "File $file not cached, downloading..."
-    echo Running wget "http://download.geteventstore.com/binaries/$file" -O "$HOME/downloads/$file"
-    wget "http://download.geteventstore.com/binaries/$file" -O "$HOME/downloads/$file"
+    echo Running wget "https://eventstore.org/downloads/$file" -O "$HOME/downloads/$file"
+    wget "https://eventstore.org/downloads/$file" -O "$HOME/downloads/$file"
 else
     echo "File $file found in cache"
 fi
